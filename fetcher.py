dc6c549075868b8a0c94e1af88ad2fc6abc22e9a
Derrick James Biana <djmbiana@gmail.com>
2026-05-31 19:13:41 +0800

    Added core loop

--- fetcher.py
+++ fetcher.py
@@ -7,9 +7,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API = os.getenv("API_KEY")
-artist = "The Cure"
-
+artist = input("Search for an artist: ")
 url = f"https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={artist}&api_key={API}&format=json"
 
 response = requests.get(url)
+parser = response.json()
 
+artist_name = parser["artist"]["name"]
+listeners = parser["artist"]["stats"]["listeners"]
+play_count = parser["artist"]["stats"]["playcount"]
+artist_summary = parser["artist"]["bio"]["summary"]
+
+print(artist_name)
+print(listeners)
+print(play_count)
+print(artist_summary)
